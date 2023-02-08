039011fbd2415a01e84668cafcd366beea3d3d2b
Tedeeeee <gksrmf359@gmail.com>
2023-02-08 18:38:50 +0900

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,16 @@
+package practice;
+
+import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		
+	}
+	
+}
+
