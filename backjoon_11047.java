17bd9415aea0270924d2fab518e2b88f9040fdfd
gunbam <118357403+Tedeeeee@users.noreply.github.com>
2023-02-17 20:06:43 +0900

    Add files via upload

---
+++ backjoon_11047.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class backjoon_11047 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st =new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int coin = Integer.parseInt(st.nextToken());
+		Integer[] arr = new Integer[N];
+		
+		for(int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		
+		Arrays.sort(arr, Collections.reverseOrder());
+
+		int Y = 0;
+		for(int i = 0; i < N; i++) {
+			if(arr[i] <= coin) {
+				Y += coin / arr[i]; 
+				coin = coin % arr[i];
+			}
+		}
+		
+		System.out.println(Y);
+	}
+
+}
