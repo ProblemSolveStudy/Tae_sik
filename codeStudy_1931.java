eb6761b96c404392f9dcde89355bfddcccbf069c
gunbam <118357403+Tedeeeee@users.noreply.github.com>
2023-02-21 11:07:36 +0900

    Add files via upload

--- codeStudy_1931.java
+++ codeStudy_1931.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.StringTokenizer;
 
 public class codeStudy_1931 {
@@ -12,23 +13,34 @@ public class codeStudy_1931 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		int N = Integer.parseInt(br.readLine());
-		int[] arr = new int[N];
+		int[][] arr = new int[N][2];
 		
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		StringTokenizer st;
 		for(int i = 0; i < N; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
+			st = new StringTokenizer(br.readLine(), " ");
+			arr[i][0] = Integer.parseInt(st.nextToken());
+			arr[i][1] = Integer.parseInt(st.nextToken());
 		}
 		
-		Arrays.sort(arr);
+		Arrays.sort(arr, new Comparator<int[]>() {
+			public int compare(int[] e1, int[] e2) {
+				if(e1[1] == e2[1]) {
+					return e1[0] - e2[0];
+				} else {
+					return e1[1] - e2[1];
+				}
+			}
+		});
 		
-		int sum = 0;
 		int prev = 0;
+		int count = 0;
 		for(int i = 0; i < arr.length; i++) {
-			prev += sum + arr[i];
-			sum += arr[i];
+			if(prev <= arr[i][0]) {
+				prev = arr[i][1];
+				count++;
+			}
 		}
-		
-		System.out.println(prev);
+		System.out.println(count);
 	}
 
 }
