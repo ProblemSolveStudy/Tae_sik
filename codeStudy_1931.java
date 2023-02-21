955c88f67f803a658fd2399262688de67f0e872d
gunbam <118357403+Tedeeeee@users.noreply.github.com>
2023-02-21 10:48:15 +0900

    Add files via upload

---
+++ codeStudy_1931.java
@@ -0,0 +1,34 @@
+package practice;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class codeStudy_1931 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[N];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		for(int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(arr);
+		
+		int sum = 0;
+		int prev = 0;
+		for(int i = 0; i < arr.length; i++) {
+			prev += sum + arr[i];
+			sum += arr[i];
+		}
+		
+		System.out.println(prev);
+	}
+
+}
